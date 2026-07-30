252104fc79f92161c579fab291302f877b1a8dae
Nikolaos Perris <nikolaos.perris@egitron.pt>
2026-07-30 15:17:54 +0100

    test: replace deprecated MockBean annotations

--- services/booking-service/src/test/java/pt/hotelbooking/booking/api/DiscountCodeControllerTests.java
+++ services/booking-service/src/test/java/pt/hotelbooking/booking/api/DiscountCodeControllerTests.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import pt.hotelbooking.booking.service.DiscountCodeService;
 
@@ -22,7 +22,7 @@ class DiscountCodeControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private DiscountCodeService discountCodeService;
 
     @Test
--- services/booking-service/src/test/java/pt/hotelbooking/booking/api/ReservationControllerTests.java
+++ services/booking-service/src/test/java/pt/hotelbooking/booking/api/ReservationControllerTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import pt.hotelbooking.booking.service.ReservationService;
 import pt.hotelbooking.booking.model.dto.ReservationResponse;
@@ -34,7 +34,7 @@ class ReservationControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private ReservationService reservationService;
 
     @Test
